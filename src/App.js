9b039affd8d4593e7f6a3bf91c10a9796386dd87
Manvi Sharma <manvis@geekyants.com>
2019-11-20 22:55:02 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+const ParentComponent = () => {
+  return (
+    <div>
+      <div>This is parent component</div>
+      <ChildComponent />
+    </div>
+  );
+};
+
+const ChildComponent = () => {
+  return <div>This is child component</div>;
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Sample app
+      <ParentComponent />
     </div>
   );
 }
