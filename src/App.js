0be1cfb0e520875a4b24a63dd8483a7eecfeb08c
Manvi Sharma <manvis@geekyants.com>
2019-11-20 23:35:42 +0530

    deleted unwanted files

--- src/App.js
+++ src/App.js
@@ -1,20 +1,6 @@
 import React from "react";
-import logo from "./logo.svg";
+
 import "./App.css";
-
-const ParentComponent = props => {
-  return (
-    <div>
-      <div>This is parent component</div>
-      <ChildComponent add={props.add} />
-    </div>
-  );
-};
-
-const ChildComponent = props => {
-  props.add(1, 2);
-  return <div>This is child component</div>;
-};
 
 function App() {
   const add = (a, b) => {
@@ -25,7 +11,7 @@ function App() {
   return (
     <div className="App">
       Sample app
-      <ParentComponent add={add} />
+      <p>Sample text</p>
     </div>
   );
 }
