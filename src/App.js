5031815a08233a423875b0d1083e1887d17a6125
Manvi Sharma <manvis@geekyants.com>
2019-11-25 23:05:49 +0530

    added routing and redux

--- src/App.js
+++ src/App.js
@@ -1,13 +1,47 @@
 import React from "react";
-
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import "./App.css";
+import Login from "../src/routes/Login";
+import PhotoDashboard from "../src/routes/PhotoDashboard";
+import About from "../src/routes/About";
+import SignUp from "../src/routes/SignUp";
 
 function App() {
   return (
-    <div className="App">
-      Sample app
-      <p>Sample text</p>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Login</Link>
+            </li>
+            <li>
+              <Link to="/photoDashboard">PhotoDashboard</Link>
+            </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
+            <li>
+              <Link to="/signUp">SignUp</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route exact path="/">
+            <Login />
+          </Route>
+          <Route exact path="/signUp">
+            <SignUp />
+          </Route>
+          <Route exact path="/photoDashboard">
+            <PhotoDashboard />
+          </Route>
+          <Route exact path="/about">
+            <About />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
