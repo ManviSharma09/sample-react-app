ed5fb8b863f22897d2f15050b1ea9178fb8933ea
Manvi Sharma <manvis@geekyants.com>
2019-12-01 02:49:59 +0530

    implemented hooks

--- src/routes/SignUp/index.js
+++ src/routes/SignUp/index.js
@@ -1,5 +1,4 @@
 import React from "react";
-// import { connect } from "react-redux";
 import { useSelector, useDispatch } from 'react-redux';
 import {
   INCREMENT_COUNT,
@@ -7,38 +6,28 @@ import {
   SET_COUNT
 } from "../../reducers/constants";
 
-// const SignUp = ({ count, increment, decrement, setCount }) => {
 const SignUp = () => {
-  const count=useSelector(state=>state.count);
-  const dispatch=useDispatch();
+
+  const {count} = useSelector(state => ({count:state.photoEditor.count}))
+  const dispatch = useDispatch();
+
+  const increment = () => dispatch({ type: INCREMENT_COUNT })
+  const decrement = () => dispatch({ type: DECREMENT_COUNT })
+  const setCount =  e => dispatch({type:SET_COUNT,payload:{count:e.target.value}})
+
   return (
     <div>
       <h2>Sign Up</h2>
       <div>{count}</div>
-      {/* <button onClick={increment}>IncrementCount</button> */}
-      <button onClick={ ()=>dispatch({ type: INCREMENT_COUNT })}>IncrementCount</button>
-      {/* <button onClick={decrement}>DecrementCount</button> */}
-      <button onClick={ ()=>dispatch({ type: DECREMENT_COUNT })}>DecrementCount</button>
+      <button onClick={increment}>IncrementCount</button>
+      <button onClick={decrement}>DecrementCount</button>
       <input
         type="number"
-        onChange={e => dispatch({type:SET_COUNT,payload:{count:e.target.value}})}
+        onChange={setCount}
         value={count}
       />
     </div>
   );
 };
 
-// const mapStateToProps = state => {
-//   return { count: state.photoEditor.count };
-// };
-
-// const mapDisPatchToProps = dispatch => {
-//   return {
-    // increment: () => dispatch({ type: INCREMENT_COUNT }),
-//     decrement: () => dispatch({ type: DECREMENT_COUNT }),
-//     setCount: count => dispatch({ type: SET_COUNT, payload: { count } })
-//   };
-// };
-
-// export default connect(mapStateToProps, mapDisPatchToProps)(SignUp);
 export default SignUp;
